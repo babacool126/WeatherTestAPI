5b5f3b9b6585fa6c64ac3edb6719360f25162c55
babacool126 <75642109+babacool126@users.noreply.github.com>
2023-09-11 19:29:53 +0200

    New weather api testapp

---
+++ Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherAPI.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Weather { get; set; }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherAPI.Models
+{
+    public class Country
+    {
+    public int Id { get; set; }
+        public string Name { get; set; }
+        public List<City> Cities { get; set; }
+    }
+}
+}
